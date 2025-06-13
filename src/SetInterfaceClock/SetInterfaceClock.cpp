1791801858707abb2d85b09380f0a0ae70453004
Jens Maus <mail@jens-maus.de>
2025-06-13 10:03:34 +0200

    replace more incorrect "unsigned int" data type uses for string search function uses which potentially could result in certain issues for 64bit compiled versions.

--- src/SetInterfaceClock/SetInterfaceClock.cpp
+++ src/SetInterfaceClock/SetInterfaceClock.cpp
@@ -65,12 +65,12 @@ int main(int argc, char* argv[])
 
 std::string trim(const std::string& str)
 {
-    unsigned int first = str.find_first_not_of(' ');
+    std::string::size_type first = str.find_first_not_of(' ');
     if (std::string::npos == first)
     {
         return str;
     }
-    unsigned int last = str.find_last_not_of(' ');
+    std::string::size_type last = str.find_last_not_of(' ');
     return str.substr(first, (last - first + 1));
 }
 
