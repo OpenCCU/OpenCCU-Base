9b1faf420b8fab6179ca71291c8f1607056f3aa1
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-09-01 10:19:57 +0200

    SPHM-1352 - HmIP-DLP(-A) - Control for the parameter LOCK_TARGET_LEVEL added (chn. 12) - the final texts of the options and a help text are still missing.

--- src/webui/www_source/ise/js/iseDoorLockTransceiver.js
+++ src/webui/www_source/ise/js/iseDoorLockTransceiver.js
@@ -3,20 +3,54 @@ iseDoorLockTransceiver.prototype = {
 
   initialize: function(chId, opts)
   {
-    var self = this,
-    metaRampTime;
-    console.log("iseDoorLockTransceiver", opts);
+    conInfo("iseDoorLockTransceiver", opts);
     this.id = chId;
     this.opts = opts;
+
+    this.paramID = this.opts.paramID;
+    this.arOptions = this.opts.options.split(",");
     this.initElem();
   },
 
   initElem: function() {
+    var self = this;
     this.btnLockTargetLevel = jQuery("#"+this.id+"lockTargetLevel");
 
     this.btnLockTargetLevel.on("click", function() {
-      console.log("Set Target Level");
+      self.getDialog();
+      self.selOptionElem = jQuery("#"+self.id+"selOptionElem");
     });
-  }
+  },
+  
+  getDialog: function() {
+    var self = this,
+      sOutput = this.getDialogHtml();
+
+    var dlg = new YesNoDialog(translateKey("dialogSetDLPTargetLevel"), sOutput, function(result) {
+
+      if (result == YesNoDialog.RESULT_YES) {
+        setDpState(self.paramID, self.selOptionElem.val() );
+      }
+
+    },"html");
+  },
+
+  getDialogHtml: function() {
+    var html = "";
+
+    html += "<table align='center'>";
+      html += "<tr>";
+        html += "<td>";
+          html += "<select id='"+this.id+"selOptionElem'>";
+          jQuery.each(this.arOptions, function(index, opt) {
+            html += "<option value='"+index+"'>"+opt+"</option>";
+          });
+          html += "</select>";
+        html += "</td>";
+      html += "</tr>";
+    html += "</table>";
+
+    return html;
+  },
 
 };
