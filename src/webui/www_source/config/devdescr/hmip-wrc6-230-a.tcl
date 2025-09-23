a5d35e225d3a1819351a9e63f1255464bd0aa112
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-09-23 13:50:42 +0200

    HmIP-WRC6-230(-A) - The orange channel markers corrected

--- src/webui/www_source/config/devdescr/hmip-wrc6-230-a.tcl
+++ src/webui/www_source/config/devdescr/hmip-wrc6-230-a.tcl
@@ -20,26 +20,16 @@ set P ""
 
 lappend P {"1" 1 0.3 0.358 0.025}
 lappend P {"2" 1 0.705 0.315 0.025}
-
 lappend P {"3" 1 0.3 0.53 0.025}
 lappend P {"4" 1 0.705 0.495 0.025}
-
 lappend P {"5" 1 0.3 0.706 0.025}
 lappend P {"6" 1 0.705 0.671 0.025}
 
-
-lappend P {"1" 5 '1'}
-lappend P {"2" 5 '2'}
-lappend P {"3" 5 '3'}
-lappend P {"4" 5 '4'}
-lappend P {"5" 5 '5'}
-lappend P {"6" 5 '6'}
-
-lappend P {"12" 5 '1'}
-lappend P {"13" 5 '2'}
-lappend P {"14" 5 '3'}
-lappend P {"15" 5 '4'}
-lappend P {"16" 5 '5'}
-lappend P {"17" 5 '6'}
+lappend P {"12" 1 0.3 0.358 0.025}
+lappend P {"13" 1 0.705 0.315 0.025}
+lappend P {"14" 1 0.3 0.53 0.025}
+lappend P {"15" 1 0.705 0.495 0.025}
+lappend P {"16" 1 0.3 0.706 0.025}
+lappend P {"17" 1 0.705 0.671 0.025}
 
 lappend P {"18" 5 '1' '2' '3' '4' '5' '6'}
