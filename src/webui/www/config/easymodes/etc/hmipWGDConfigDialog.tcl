70230244b6cc57a7f73589b309a7c8db31ed2fb1
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-11-05 14:45:12 +0100

    Translation

--- src/webui/www/config/easymodes/etc/hmipWGDConfigDialog.tcl
+++ src/webui/www/config/easymodes/etc/hmipWGDConfigDialog.tcl
@@ -520,7 +520,7 @@ proc getDisplayInputTransmitter {chn p descr} {
       set options(10) "\${optionAccessDayNight}"
       set options(11) "\${optionAccessOpen}"
 
-      append html  "<td>[getOptionBox '$param' options $ps($param) $chn $prn]&nbsp;[getHelpIcon $param 550 400]</td>"
+      append html  "<td>[getOptionBox '$param' options $ps($param) $chn $prn]&nbsp;[getHelpIcon $param\_A 550 500]</td>"
     append html "</tr>"
   }
 
