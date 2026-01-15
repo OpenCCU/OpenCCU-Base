d617785034faa9e36b92de5bda5bb7f488f7fb19
cn-eq3 <211333494+cn-eq3@users.noreply.github.com>
2026-01-15 15:05:13 +0100

    libUnifiedLanComm: Add license header

--- src/hss_led/hss_led_util.cpp
+++ src/hss_led/hss_led_util.cpp
@@ -1,5 +1,9 @@
 /*
-* Copyright (C) 2018 eQ-3 Entwicklung GmbH
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
 */
 #include "hss_led_util.h"
 #include <sstream>
--- src/hss_led/hss_led_util.h
+++ src/hss_led/hss_led_util.h
@@ -1,5 +1,9 @@
 /*
-* Copyright (C) 2018 eQ-3 Entwicklung GmbH
+* Copyright 2025 eQ-3 AG - All Rights Reserved.
+* 
+* Licensed under the HMSL 2 (the "License"). You may not use
+* this file except in compliance with the License.  You can obtain a copy
+* in the file HMSL.txt in the source distribution.
 */
 #ifndef _HSS_LED_UTIL_H_
 #define _HSS_LED_UTIL_H_ 
