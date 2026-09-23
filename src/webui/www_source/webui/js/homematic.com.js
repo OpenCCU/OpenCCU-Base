37a4172a8b48bce59b3c0e1d2ad93d4eff6ed314
Jens Maus <mail@jens-maus.de>
2026-09-23 15:46:25 +0200

    fix(webui): use HTTPS for firmware downloads
    Signed-off-by: Jens Maus <mail@jens-maus.de>

--- src/webui/www_source/webui/js/homematic.com.js
+++ src/webui/www_source/webui/js/homematic.com.js
@@ -18,8 +18,8 @@ homematic.com =
     this.m_ccuProduct = getProduct();
     this.preURL = (this.m_ccuProduct < 3) ? "" : "ccu3-";
     this.m_product ="HM-CCU" + this.m_ccuProduct;
-    this.m_URLServer = (isHTTPS) ? "https://"+this.preURL+"update.homematic.com:8443" : "http://"+this.preURL+"update.homematic.com";
-    this.m_fieldTestURLServer = (isHTTPS) ? "https://fieldtest-ccu3-update.homematic.com" : "http://fieldtest-ccu3-update.homematic.com";
+    this.m_URLServer = "https://"+this.preURL+"update.homematic.com";
+    this.m_fieldTestURLServer = "https://fieldtest-ccu3-update.homematic.com";
 
     this.serial = homematic("CCU.getSerial");
     this.serial = ((this.serial != "") && (typeof this.serial != "undefined") && (this.serial != null)) ? this.serial : "0";
